02f8393a274320a64c5d70ecaa98bcdebf842147
onesunny2 <ippne996@gmail.com>
2024-07-11 13:56:26 +0900

    file setting

--- DDADDA/DDADDAApp.swift
+++ DDADDA/DDADDAApp.swift
@@ -13,7 +13,7 @@ struct DDADDAApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SelectView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
---
+++ DDADDA/Extension/Extension+Font.swift
@@ -0,0 +1,38 @@
+//
+//  Extension+Font.swift
+//  DDADDA
+//
+//  Created by Lee Wonsun on 7/5/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static func santokkiFont(size: CGFloat) -> Font {
+        return Font.custom("HSSantokki", size: size)
+    }
+    
+    static var menu: Font {
+        return santokkiFont(size: 65)
+    }
+    
+    static var title1: Font {
+        return santokkiFont(size: 45)
+    }
+    
+    static var title2: Font {
+        return santokkiFont(size: 42)
+    }
+    
+    static var badge: Font {
+        return santokkiFont(size: 30)
+    }
+    
+    static var subTitle: Font {
+        return santokkiFont(size: 24)
+    }
+    
+    static var itemTitle: Font {
+        return santokkiFont(size: 20)
+    }
+}
