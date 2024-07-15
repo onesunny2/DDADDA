90b2c0c37f1b0220c4093be6c59df448a1fc4486
onesunny2 <ippne996@gmail.com>
2024-07-15 12:16:47 +0900

    [Feature] #3 - selectView 표지판 버튼 UI 작업 및 클릭 시 navigation 연결

---
+++ DDADDA/View/LibraryView.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryView.swift
+//  DDADDA
+//
+//  Created by Lee Wonsun on 7/15/24.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text("Library")
+    }
+}
+
+#Preview {
+    LibraryView()
+}
---
+++ DDADDA/View/SketchView.swift
@@ -0,0 +1,18 @@
+//
+//  SketchView.swift
+//  DDADDA
+//
+//  Created by Lee Wonsun on 7/15/24.
+//
+
+import SwiftUI
+
+struct SketchView: View {
+    var body: some View {
+        Text("Sketch")
+    }
+}
+
+#Preview {
+    SketchView()
+}
