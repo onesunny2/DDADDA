4cf90c14470bf3855e3c2f06ba1f05cf2d99ec94
onesunny2 <ippne996@gmail.com>
2024-10-31 23:57:35 +0900

    [Feat] #43 savedDrawing 이미지 조절 구현

--- DDADDA/View/SketchView/SketchView.swift
+++ DDADDA/View/SketchView/SketchView.swift
@@ -161,7 +161,7 @@ private struct SketchBook: View {
     
     var body: some View {
         VStack(alignment: .leading) {
-            VStack(spacing: -30) {
+            VStack(spacing: -65) {
                 Image(.sketchbookSpring)
                     .frame(width: size.width * 0.41)
                     .zIndex(1)
@@ -186,7 +186,11 @@ private struct SketchBook: View {
                         Image(uiImage: savedDrawing)
                             .resizable()
                             .aspectRatio(contentMode: .fit)
-                            .frame(width: size.height * 1)
+                            .frame(height: size.height * 0.4)
+                    } else {
+                        Rectangle()
+                            .foregroundStyle(.clear)
+                            .frame(width: size.width * 0.39, height: size.height * 0.4)
                     }
                 }
             }
